f126f7a1eefec0ae8588923d67bb061d681976ff
Z-MohamedGamal <mohamedgamal0.02510@gmail.com>
2025-11-30 22:40:55 +0200

    Delete src directory

--- src/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-export class CustomError extends Error {
-    constructor(message, statusCode, success = false) {
-        super(message);
-        this.statusCode = statusCode;
-        this.success = success;
-    }
-}
